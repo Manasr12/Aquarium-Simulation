8c51560595b4d6913bc35fd671d037f52f7c7bf6
Mannan Dhillon <74431005+Manasr12@users.noreply.github.com>
2024-03-24 23:36:36 -0400

    Add files via upload

---
+++ AquariumApp.h
@@ -0,0 +1,21 @@
+/**
+ * @file AquariumApp.h
+ * @author Mannan Dhillon
+ *
+ * initializes the application
+ */
+
+#ifndef AQUARIUM_AQUARIUMAPP_H
+#define AQUARIUM_AQUARIUMAPP_H
+
+
+
+/**
+ * Main application class
+ */
+class AquariumApp : public wxApp {
+public:
+    bool OnInit() override;
+};
+
+#endif //AQUARIUM_AQUARIUMAPP_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+/**
+ * @file main.cpp
+ * @author Mannan Dhillon
+ */
+#include "pch.h"
+#include "AquariumApp.h"
+
+wxIMPLEMENT_APP(AquariumApp);
